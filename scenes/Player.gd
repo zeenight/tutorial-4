6a41609da963e0917aba59ccf8f508049ffb77d2
zeenight <146532301+zeenight@users.noreply.github.com>
2026-03-06 09:38:42 +0700

    Initial commit

---
+++ scenes/Player.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D
+
+@export var speed: int = 400
+@export var gravity: int = 1200
+@export var jump_speed: int = -400
+
+
+func get_input():
+	velocity.x = 0
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.y = jump_speed
+	if Input.is_action_pressed("right"):
+		velocity.x += speed
+	if Input.is_action_pressed("left"):
+		velocity.x -= speed
+
+
+func _physics_process(delta):
+	velocity.y += delta * gravity
+	get_input()
+	move_and_slide()
+
+
+func _process(_delta):
+	if not is_on_floor():
+		$Animator.play("Jump")
+	elif velocity.x != 0:
+		$Animator.play("Walk")
+	else:
+		$Animator.play("Idle")
+
+	if velocity.x != 0:
+		if velocity.x > 0:
+			$Sprite2D.flip_h = false
+		else:
+			$Sprite2D.flip_h = true
