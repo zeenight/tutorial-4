500850c847486a5a8c95bae55a39731155458758
zeenight <zillan.lambot@gmail.com>
2026-03-27 01:04:27 +0700

    added feature 2 of tutorial 6

--- scenes/WinCondition.gd
+++ scenes/WinCondition.gd
@@ -4,4 +4,4 @@ extends Area2D
 
 func _on_Area_Trigger_body_entered(body):
 	if body.get_name() == "Player":
-		get_tree().change_scene_to_file(str("res://scenes/" + sceneName + ".tscn"))
+		Transition.change_scene(str("res://scenes/" + sceneName + ".tscn"))
